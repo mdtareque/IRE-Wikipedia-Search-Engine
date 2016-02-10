16de2be3cbfb3c8d82a17c988dde33c649810a52
mtk <mohdtarequekhan@gmail.com>
2016-02-10 05:57:14 +0530

    Wed Feb 10 05:57:14 IST 2016 syncing

--- git-push.sh
+++ git-push.sh
@@ -8,8 +8,8 @@ if [ $# -ne 0 ]; then
     commit_msg="`date` $1"
 fi
 
-echo git commit -m \"$commit_msg\"
-git commit -m \"$commit_msg\"
+echo git commit -m "`echo $commit_msg`"
+git commit -m "`echo $commit_msg`"
 
 echo git push -u origin master
 git push -u origin master
